b829af58e3c21269846845ae9fd130b87b8b9aa8
unknownSRE <hitxpert@gmail.com>
2026-01-16 10:35:14 +0530

    rating part completed

---
+++ frontend-2/src/app/components/rating/rating.ts
@@ -0,0 +1,62 @@
+import { Component, Input, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RatingService } from '../../services/rating';
+import { Rating } from '../../models/rating.model';
+
+@Component({
+  selector: 'app-rating',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.scss'],
+})
+export class RatingComponent {
+  private ratingService = inject(RatingService);
+
+  @Input() rideId!: number;
+  @Input() givenBy!: number;
+  @Input() givenTo!: number;
+
+  stars = [1, 2, 3, 4, 5];
+  selectedScore = signal<number>(0);
+  hoveredScore = signal<number | null>(null);
+  comment = '';
+  isSubmitting = signal(false);
+
+  setRating(score: number) {
+    this.selectedScore.set(score);
+  }
+
+  setHover(score: number | null) {
+    this.hoveredScore.set(score);
+  }
+
+  submit() {
+    if (this.selectedScore() === 0) {
+      alert('Please select a rating before submitting.');
+      return;
+    }
+
+    this.isSubmitting.set(true);
+
+    const newRating: Rating = {
+      ride_id: this.rideId,
+      given_by: this.givenBy,
+      given_to: this.givenTo,
+      score: this.selectedScore(),
+      comment: this.comment,
+    };
+
+    this.ratingService.submitRating(newRating).subscribe({
+      next: (res) => {
+        alert('Rating submitted successfully!');
+        this.isSubmitting.set(false);
+      },
+      error: (err) => {
+        console.error('Submission failed', err);
+        this.isSubmitting.set(false);
+      },
+    });
+  }
+}
